4bc347a3ff2404cac2e7112f377d65e32b55ee93
Walk <2757412961@qq.com>
2021-04-09 16:45:42 +0800

    文档：个人素材上传和查询

--- sishi-manager/src/test/java/cn/edu/zju/sishi/entity/vo/TagCompareTimeTest.java
+++ sishi-manager/src/test/java/cn/edu/zju/sishi/entity/vo/TagCompareTimeTest.java
@@ -11,21 +11,21 @@ public class TagCompareTimeTest extends TestCase {
     @Test
     public void test01() {
 
-        TagCompareTime tagCompareTime01 = new TagCompareTime("asd", "sd", "d", "as", "1829nian745", null, null);
-        TagCompareTime tagCompareTime02 = new TagCompareTime("asd", "ad", "asd", "asd", "1848asd", null, null);
-        TagCompareTime tagCompareTime03 = new TagCompareTime("asd", "ad", "asd", "asd", "1811asd", null, null);
-
-        System.out.println("1829nian745".compareTo("1848asd"));
-        System.out.println(tagCompareTime01.compareTo(tagCompareTime02));
-
-        ArrayList<TagCompareTime> objects = new ArrayList<>();
-        objects.add(tagCompareTime01);
-        objects.add(tagCompareTime02);
-        objects.add(tagCompareTime03);
-
-        System.out.println(objects);
-        Collections.sort(objects);
-        System.out.println(objects);
+//        TagCompareTime tagCompareTime01 = new TagCompareTime("asd", "sd", "d", "as", "1829nian745", null, null);
+//        TagCompareTime tagCompareTime02 = new TagCompareTime("asd", "ad", "asd", "asd", "1848asd", null, null);
+//        TagCompareTime tagCompareTime03 = new TagCompareTime("asd", "ad", "asd", "asd", "1811asd", null, null);
+//
+//        System.out.println("1829nian745".compareTo("1848asd"));
+//        System.out.println(tagCompareTime01.compareTo(tagCompareTime02));
+//
+//        ArrayList<TagCompareTime> objects = new ArrayList<>();
+//        objects.add(tagCompareTime01);
+//        objects.add(tagCompareTime02);
+//        objects.add(tagCompareTime03);
+//
+//        System.out.println(objects);
+//        Collections.sort(objects);
+//        System.out.println(objects);
 
     }
 
