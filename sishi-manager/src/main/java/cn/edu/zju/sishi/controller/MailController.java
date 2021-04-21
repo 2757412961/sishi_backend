f256f833d7f1fdc67552ff65945eea7f4003c35c
赵佳晖 <1134874524@qq.com>
2021-04-21 21:49:40 +0800

    Accept Merge Request #44: (zjh-test -> master) Merge Request: Title: [Append] 答题登陆文档修改 Created By: @赵佳晖 Reviewed By: @lemon Accepted By: @赵佳晖 URL: https://zittur.coding.net/p/sishi-backend/d/sishi_backend/git/merge/44

--- sishi-manager/src/main/java/cn/edu/zju/sishi/controller/MailController.java
+++ sishi-manager/src/main/java/cn/edu/zju/sishi/controller/MailController.java
@@ -11,10 +11,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 
@@ -42,7 +39,7 @@ public class MailController {
     @Autowired
     private RedisService redisService;
 
-    public static final int REDIS_EXPIRE_TIME = 120;
+    public static final int REDIS_EXPIRE_TIME = 600;
 
     /**
      * @Description: 发送验证码
@@ -53,15 +50,19 @@ public class MailController {
      */
     @GetMapping("/sendCaptcha")
     public MailResponse sendCaptcha(@RequestParam("emailAddress") String emailAddress) {
-        MailResponse mailResponse = new MailResponse();
+        try{
+            MailResponse mailResponse = new MailResponse();
 
-        log.info("Start invoke sendCaptcha()");
-        Integer captcha = 100000 + new Random().nextInt(899999);
-        redisService.set(RedisKeys.REDIS_Captcha_PREFIX + emailAddress, captcha.toString(), REDIS_EXPIRE_TIME);
-        mailService.sendCaptcha(emailAddress, captcha);
+            log.info("Start invoke sendCaptcha()");
+            Integer captcha = 100000 + new Random().nextInt(899999);
+            redisService.set(RedisKeys.REDIS_Captcha_PREFIX + emailAddress, captcha.toString(), REDIS_EXPIRE_TIME);
+            mailService.sendCaptcha(emailAddress, captcha);
 
-        mailResponse.setMessage("发送成功，验证码将在120秒后过期");
-        return mailResponse;
+            mailResponse.setMessage("发送成功，验证码将在5分钟后过期");
+            return mailResponse;
+        }catch (Exception e){
+            throw new ValidationException("邮箱发送失败");
+        }
     }
 
     /**
@@ -72,7 +73,7 @@ public class MailController {
      * @Date: 2021/4/6
      */
     @Transactional
-    @GetMapping("/resetPassword")
+    @PutMapping("/resetPassword")
     public MailResponse resetPasswordByEmail(@RequestParam("emailAddress") String emailAddress) {
         MailResponse mailResponse = new MailResponse();
 
@@ -94,7 +95,7 @@ public class MailController {
      * @Date: 2021/4/6
      */
     @Transactional
-    @GetMapping("/updatePassword")
+    @PutMapping("/updatePassword")
     public MailResponse updatePasswordByEmail(@RequestParam("captcha") String captcha,
                                               @RequestParam("emailAddress") String emailAddress,
                                               @RequestParam("newPassword") String newPassword) {
@@ -111,9 +112,26 @@ public class MailController {
         }
         mailService.sendResetPassword(emailAddress, newPassword);
 
-        mailResponse.setMessage("根据邮箱修改密码：1.发送验证码 2.填入验证码和新的密码 3.修改成功");
+        /** mailResponse.setMessage("根据邮箱修改密码：1.发送验证码 2.填入验证码和新的密码 3.修改成功"); */
+        mailResponse.setMessage("密码重置成功！");
         return mailResponse;
     }
 
+    @PostMapping("/checkCaptcha")
+    public MailResponse checkCaptcha(@RequestParam("captcha") String captcha, @RequestParam("emailAddress") String emailAddress) {
+        MailResponse mailResponse = new MailResponse();
+
+        log.info("Start invoke updatePasswordByEmail()");
+        String redisCaptcha = redisService.get(RedisKeys.REDIS_Captcha_PREFIX + emailAddress);
+        if (StringUtils.isEmpty(redisCaptcha)) {
+            throw new ValidationException("验证码失效");
+        }
+        if (!captcha.equals(redisCaptcha)) {
+            throw new ValidationException("验证码错误");
+        }
+
+        mailResponse.setMessage("验证码正确");
+        return mailResponse;
+    }
 
 }
